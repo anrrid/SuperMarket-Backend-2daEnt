33a188379c46c205910e6095f075dfdb48b6463e
Antonella Rodríguez <82007150+anrrid@users.noreply.github.com>
2022-06-14 07:20:57 -0300

    Fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 // const routesCart = require("./routes/routes2")
 const routesProd = require("./routes/routes")
+const { mongoose } = require("./config/mongo");
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -10,14 +11,17 @@ app.use(express.static('./public'))
 const errObj = { error: 'Producto no encontrado' };
 const err401 = { error: 'No estás autorizado para acceder a ésta URL' }
 
-const admin = true;
+// const admin = true;
 
 // app.use('/api/cart', routesCart)
 app.use('/api/products', routesProd);
 
 
-const PORT = process.env.PORT || 8080;
-const server = app.listen(PORT, () => {
-    console.log(`El server está a la escucha en el puerto ${server.address().port}`);
-});
-server.on('error', error => console.log(`Error al iniciar el servidor: ${error}`));
+const init = async () => {
+    await mongoose();
+    const puerto = process.env.PORT || 8080;
+
+    server.listen(puerto, () => console.log(`SERVIDOR ENCENDIDO EN EL PUERTO ${puerto}`));
+};
+
+init();
